ceed34912ac58292a77012c6d3ea42ef3dd7623c
peteryouseph <peteryouseph@gmail.com>
2024-05-26 14:07:36 -0400

    fix: corrige chamadas assíncronas do Dynamo visando melhorar requests para o serviço

--- api/services/inferenceLogsToDynamo.py
+++ api/services/inferenceLogsToDynamo.py
@@ -1,8 +1,9 @@
 from datetime import datetime, timezone
 import json
-import aioboto3
+import boto3
 import os
 from dotenv import load_dotenv
+from botocore.exceptions import ClientError
 
 # Carrega as variáveis de ambiente
 load_dotenv()
@@ -11,21 +12,25 @@ load_dotenv()
 DYNAMODB_TABLE_NAME = os.getenv('DYNAMODB_TABLE_NAME')
 AWS_REGION = os.getenv('AWS_REGION')
 
-# Inicializa o cliente boto3
-dynamodb_resource = aioboto3.resource('dynamodb', region_name=AWS_REGION)
+def log_access(request_data: dict, response_data: dict):
+    # Inicializa o recurso do DynamoDB
+    dynamodb = boto3.resource('dynamodb', region_name=AWS_REGION)
+    table = dynamodb.Table(DYNAMODB_TABLE_NAME)
 
-async def log_access(request_data: dict, response_data: dict):
-    async with dynamodb_resource.Table(DYNAMODB_TABLE_NAME) as table:
-        # Gerar um identificador único para o log
-        log_id = str(datetime.now(timezone.utc).timestamp()).replace('.', '')
+    # Gerar um identificador único para o log
+    log_id = str(datetime.now(timezone.utc).timestamp()).replace('.', '')
 
-        # Preparar o item de log
-        item = {
-            'log_id': log_id,
-            'timestamp': datetime.now(timezone.utc).isoformat(),
-            'request': json.dumps(request_data),
-            'response': json.dumps(response_data)
-        }
+    # Preparar o item de log
+    item = {
+        'log_id': log_id,
+        'timestamp': datetime.now(timezone.utc).isoformat(),
+        'request': json.dumps(request_data),
+        'response': json.dumps(response_data)
+    }
 
-        # Inserir o item de log na tabela DynamoDB de forma assíncrona
-        await table.put_item(Item=item)
+    try:
+        # Inserir o item de log na tabela DynamoDB
+        table.put_item(Item=item)
+        print("Log de acesso registrado no DynamoDB")
+    except ClientError as e:
+        print(f"Erro ao registrar o log de acesso no DynamoDB: {e}")
