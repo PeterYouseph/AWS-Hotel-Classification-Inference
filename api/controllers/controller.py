a8b9d867b455cccd905a27e01ae82dcd202e730a
peteryouseph <peteryouseph@gmail.com>
2024-05-26 14:04:49 -0400

    fix: corrige chamadas assíncronas ao DynamoDB que estavam ocasionando em problemas na requisição

--- api/controllers/controller.py
+++ api/controllers/controller.py
@@ -2,22 +2,22 @@ from fastapi import APIRouter, HTTPException, Request
 from services.predictionService import predict
 from models.dataModel import preprocess_input
 from services.inferenceLogsToDynamo import log_access
+from services.credentialService import refresh_credentials
+from os import environ
 
 router = APIRouter()
 
 @router.post("/api/v1/predict")
-async def predict(request: Request):
+async def predict_endpoint(request: Request):
     try:
         sent_data = await request.json()
 
         processed_data = preprocess_input(sent_data)
-        response = await predict(processed_data)
+        response = predict(processed_data) 
 
-        await log_access(sent_data, response)
+        # Loga a requisição e a resposta no DynamoDB
+        log_access(sent_data, response)
 
         return response
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
-
-
-
